151d42d5811a99509bb003b3b25d80b1533025fc
Damon718 <144197240+Damon718@users.noreply.github.com>
2024-02-29 22:28:41 +0800

    update js new function

--- js/script.js
+++ js/script.js
@@ -1,8 +1,5 @@
 function validateForm(){
-    var username = document.forms["myForm"]["username"].value; 
-    var password = document.forms["myForm"]["password"].value; 
-    var email = document.forms["myForm"]["email"].value; 
-  
+
 }
 window.onload = function() {
     var html = document.getElementById("html"); 
