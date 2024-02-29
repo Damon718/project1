066f4c4ac32ca7aed6f5bf708dfc8887cc19514f
Damon718 <144197240+Damon718@users.noreply.github.com>
2024-02-29 22:29:51 +0800

    update js function

--- js/script.js
+++ js/script.js
@@ -1,5 +1,15 @@
 function validateForm(){
-
+    var username = document.forms["myForm"]["username"].value; 
+    var password = document.forms["myForm"]["password"].value; 
+    var email = document.forms["myForm"]["email"].value; 
+    
+    if(username == "" || password == ""||email==""){ 
+        alert("username or password or email cannot be empty");
+        return false; 
+    }else{
+        alert("Register Success");
+        return true; 
+    }
 }
 window.onload = function() {
     var html = document.getElementById("html"); 
